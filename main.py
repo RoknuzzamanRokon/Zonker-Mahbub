da91399b2f05b278b69d59d2cd4e79d88d337460
rokon <rokon.raz@gmail.com>
2022-09-30 04:50:34 +0600

    this is an smart scanner.Here show an image in ip camer,using url

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import urllib.request as request
-from PIL import Image
-url = 'http://192.168.0.112:8080/shot.jpg?rnd=99891'
+import numpy as np
+import cv2
+
+url = 'http://192.168.0.112:8080/shot.jpg?rnd=163311'
 
 while True:
     img = request.urlopen(url)
-    img2 = Image.open(img)
-    img2.show()
+    img_type = bytearray(img.read())
+    img_np = np.array(img_type, dtype=np.uint8)
+    frame = cv2.imdecode(img_np, -1)
+    cv2.imshow('My Smart scanner', frame)
+    cv2.waitKey(1)
     
-    s = str(input())
-    if s =='s':
-        break
+
     
+   
