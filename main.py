6a6554bcf383b41c6eb6adcb1c72d6e7b6fe2ac6
rokon <rokon.raz@gmail.com>
2022-10-01 03:23:07 +0600

    There import PIL,fram the image and save a image as pdf file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import urllib.request as request
 import numpy as np
 import cv2
+from PIL import Image
 
 url = 'http://192.168.0.110:8080/shot.jpg?rnd=274471'
 
@@ -11,6 +12,8 @@ while True:
     frame = cv2.imdecode(img_np, -1)
     cv2.imshow('My Smart scanner', frame)
     if cv2.waitKey(1) == ord('s'):
+        img_pil = Image.fromarray(frame)
+        img_pil.save('My_Scanned_file.pdf')
         print("Save")
     
 
